c009f87775a008f27e829ae7ca7a301a3aa017e3
sadending82 <sadending82@gmail.com>
2025-03-11 20:17:03 +0900

    CSS error

--- next.config.ts
+++ next.config.ts
@@ -4,6 +4,10 @@ const nextConfig: NextConfig = {
   /* config options here */
   reactStrictMode: true,
   output: "export",
+  assetPrefix:
+  process.env.NODE_ENV === "production"
+  ? "https://sadytrd.com"
+  : "",
 };
 
 export default nextConfig;
